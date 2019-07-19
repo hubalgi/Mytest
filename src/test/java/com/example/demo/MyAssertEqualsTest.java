fedb7414f6a31dffd2c6e44745e2fa5bf3f9fcd0
hemant <hemant_balgi@thbs.com>
2019-07-19 11:21:55 +0000

    some comment

---
+++ src/test/java/com/example/demo/MyAssertEqualsTest.java
@@ -0,0 +1,24 @@
+package java.com.example.demo;
+
+	import org.junit.Test;
+	 
+	import com.example.demo.service.Employee;
+	 
+	import static org.junit.Assert.*;
+	 
+	public class MyAssertEqualsTest {
+	 
+	    @Test
+	    public void mySimpleEqualsTest(){
+	         
+	        String expectedName = "Nattu";
+	        assertEquals(expectedName, Employee.getEmpNameWithHighestSalary());
+	    }
+	     
+	    @Test
+	    public void myObjectEqualsTest(){
+	         
+	        Employee expectedEmpObj = new Employee(1, "Nattu", 15000);
+	        assertEquals(expectedEmpObj, Employee.getHighestPaidEmployee());
+	    }
+	} 
